0aa9428826403943c27031eddb215f52eabf7337
deoclaw <115387064+deoclaw@users.noreply.github.com>
2023-06-08 02:56:45 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+//Q1
+//Question 1 - The variable myNumber should by defined.
+let myNumber;
+
+//Q2
+/*
+The variable myNumber should be type number.
+The variable myNumber should be equal to 5.
+*/
+myNumber = 5;
+console.log(`${myNumber} is type ${typeof(myNumber)}`);
+//Q3
+/*
+The variable myOtherNumber should by defined.
+The variable myOtherNumber should be type number.
+The variable myOtherNumber should be equal to 5.
+*/
+let myOtherNumber = 5;
+console.log(`${myOtherNumber} is type ${typeof(myOtherNumber)}`);
+//Q4
+/*
+The variable myTotal should by defined.
+The variable myTotal should be type number.
+The variable myTotal should be equal to the product of myNumber and myOtherNumber.‣
+*/
+let myTotal = myNumber + myOtherNumber;
+console.log(`${myTotal} is type ${typeof(myTotal)}`);
+
+//Q5
+/*
+he variable myName should by defined.
+The variable myName should be type string.
+The variable myCombo should by defined.
+The variable myCombo should be type string.
+The variable myCombo should be equal to the sum of myNumber and myOtherNumber.‣
+*/
+let myName = "C";;
+let myCombo = myTotal.toString();
+console.log(`${myName} is type ${typeof(myName)} and ${myCombo} is of type ${typeof(myCombo)}`);
+
+//Q6
+/*
+The variable myOtherCombo should by defined.
+The variable myOtherCombo should be type number.
+The variable myOtherCombo should be equal to the sum of myNumber and myOtherNumber.‣
+*/
+let myOtherCombo = myNumber + myOtherNumber;
+console.log(`${myOtherCombo} is type ${typeof(myOtherCombo)}`);
+
+/*Question 7
+The variable myRemainder should by defined.
+The variable myRemainder should be type number.
+The variable myRemainder should be equal to the remainder of myNumber divided by 5.
+*/
+let myRemainder = myNumber%5;
+console.log(`${myRemainder} is type ${typeof(myRemainder)}`);
+/*
+Question 8
+The variable i should by defined.
+The variable i should be type number.
+The variable i should be equal to 2.
+*/
+let i = 2;
+console.log(`${i} is type ${typeof(i)}`);
+/*
+Question 9
+The variable int should by defined.
+The variable int should be type number.
+The variable int should be equal to 4.
+*/
+let int = 4;
+console.log(`${int} is type ${typeof(int)}`);
+/*
+Question 10
+The variable myBoolean should by defined.
+The variable myBoolean should be type boolean.
+The variable myBoolean should be false.
+*/
+let myBoolean = false;
+console.log(`${myBoolean} is type ${typeof(myBoolean)}`);
